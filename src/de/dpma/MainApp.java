14dd423d9f913ee13db343f96434217529f30feb
Flo <Flo@192.168.111.145>
2017-06-09 08:03:56 +0200

    Zeug
    Flo

--- src/de/dpma/MainApp.java
+++ src/de/dpma/MainApp.java
@@ -1,52 +1,59 @@
 package de.dpma;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
-import java.util.logging.Logger;
 
 import de.dpma.dao.DatabaseConnection;
 import javafx.application.Application;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-	
+
 	public static DatabaseConnection dbcon = null;
-	
-	Logger log = Logger.getLogger(MainApp.class.getName());
-	
-	BorderPane borderPane;
-	
-	@Override
-	public void start(Stage primaryStage) {
-		
-		try {
-			FXML_GUI fxml_gui = new FXML_GUI();
-			fxml_gui.primaryStage = primaryStage;
-			fxml_gui.initRootLayout();
-			fxml_gui.showLogin();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		
+
+	// Logger log = Logger.getLogger(MainApp.class.getName());
+	//
+	// BorderPane borderPane;
+	//
+	// @Override
+	// public void start(Stage primaryStage) {
+	//
+	// try {
+	// FXML_GUI fxml_gui = new FXML_GUI();
+	// fxml_gui.primaryStage = primaryStage;
+	// fxml_gui.initRootLayout();
+	// fxml_gui.showLogin();
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
 		try {
 			dbcon = new DatabaseConnection();
 			launch(args);
-			
-		}
-		catch (ClassNotFoundException | SQLException e) {
+
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
 				dbcon.closeConnection();
-			}
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+
+		/*
+		 * System.out.println(new File(".").getAbsoluteFile()); WriteDocx docx =
+		 * new WriteDocx();
+		 */
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 }
