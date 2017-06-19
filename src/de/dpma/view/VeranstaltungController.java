96eb451c2c86ce990f69b30f69f3551d0f38be76
Flo <Flo@DESKTOP-R6OOO5U>
2017-06-19 10:42:10 +0200

    Etliche Rechtschreibfehler in Alerts gefixt
    Flo

--- src/de/dpma/view/VeranstaltungController.java
+++ src/de/dpma/view/VeranstaltungController.java
@@ -11,60 +11,60 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class VeranstaltungController {
-	
+
 	@FXML
 	TextField dozentTextField;
-	
+
 	@FXML
 	TextField aktenzeichenTextField;
-	
+
 	@FXML
 	TextField schulArtTextField;
-	
+
 	@FXML
 	DatePicker vfgDatePicker;
-	
+
 	@FXML
 	ComboBox<String> vortragComboBox;
-	
+
 	@FXML
 	DatePicker datumVonDatePicker;
-	
+
 	@FXML
 	DatePicker datumBisDatePicker;
-	
+
 	@FXML
 	ComboBox<String> euro_StdComboBox;
-	
+
 	@FXML
 	TextField stdZahlTextField;
-	
+
 	@FXML
 	TextField betragTextField;
-	
+
 	@FXML
 	TextField betrag_ABCTextField;
-	
+
 	Event event;
-	
+
 	AlertUtil alert;
-	
+
 	@FXML
 	private void inizialize() {
-		
+
 		ObservableList<String> euro_StdComboBoxList = FXCollections.observableArrayList(); // Stundesatz
 		// herausnehmen
 		euro_StdComboBox.setItems(euro_StdComboBoxList);
-		
+
 		ObservableList<String> vortragComboBoxList = FXCollections.observableArrayList("Schulung", "Sonstiges");
 		vortragComboBox.setItems(vortragComboBoxList);
 	}
-	
+
 	public void handleNew(Event event) {
-		
+
 		// Stundenlohn Tabelle die Auswahlmöglichkeiten reinschmeißen
 		this.event = event;
-		
+
 		dozentTextField.setText(event.DozentString());
 		aktenzeichenTextField.setText(event.getAktenz());
 		schulArtTextField.setText(event.getSchulart());
@@ -72,8 +72,7 @@ public class VeranstaltungController {
 		String vortrag;
 		if (event.getVortrg_mode() == 1) {
 			vortrag = "Schulung";
-		}
-		else {
+		} else {
 			vortrag = "Sonstiges";
 		}
 		vortragComboBox.setValue(vortrag);
@@ -88,53 +87,46 @@ public class VeranstaltungController {
 		String betrag_ABC = String.valueOf(event.Betrag_ABCProperty());
 		betrag_ABCTextField.setText(betrag_ABC);
 	}
-	
+
 	@FXML
 	private void handleSubmit() {
-		
+
 		if (dozentTextField.getText().isEmpty() /* || Dozent nicht Vorhanden */) {
 			alert = new AlertUtil("Dozent ungültig",
 					"Es wurde kein gültiger Dozent eingegeben. Bitte geben Sie einen validen Dozent an und versuchen Sie es erneut.",
 					"WARNING");
-		}
-		else if (aktenzeichenTextField.getText().isEmpty()) {
+		} else if (aktenzeichenTextField.getText().isEmpty()) {
 			alert = new AlertUtil("Aktenzeichen ungültig",
-					"Es wurde kein gültiges Aktenzeichen eingegeben. Bitte geben Sie einen validen Aktenzeichen an und versuchen Sie es erneut.",
+					"Es wurde kein gültiges Aktenzeichen eingegeben. Bitte geben Sie ein valides Aktenzeichen an und versuchen Sie es erneut.",
 					"WARNING");
-		}
-		else if (schulArtTextField.getText().isEmpty()) {
+		} else if (schulArtTextField.getText().isEmpty()) {
 			alert = new AlertUtil("Schulungsart ungültig",
-					"Es wurde kein gültige Schulungsart eingegeben. Bitte geben Sie einen validen Schulungsart an und versuchen Sie es erneut.",
+					"Es wurde keine gültige Schulungsart eingegeben. Bitte geben Sie eine valide Schulungsart an und versuchen Sie es erneut.",
 					"WARNING");
-		}
-		else if (vfgDatePicker.getValue().toString().isEmpty()) {
+		} else if (vfgDatePicker.getValue().toString().isEmpty()) {
 			alert = new AlertUtil("Verfügungstag ungültig",
 					"Es wurde kein gültiger Verfügungstag eingegeben. Bitte geben Sie einen validen Verfügungstag an und versuchen Sie es erneut.",
 					"WARNING");
-		}
-		else if (vortragComboBox.getValue().isEmpty()) {
-			alert = new AlertUtil("Vortrag ungültig",
-					"Es wurde kein gültiger Vortrag eingegeben. Bitte geben Sie einen validen Vortrag an und versuchen Sie es erneut.",
+		} else if (vortragComboBox.getValue().isEmpty()) {
+			alert = new AlertUtil("Vortragsart ungültig",
+					"Es wurde keine gültige Vortragsart eingegeben. Bitte geben Sie eine valide Vortragsart an und versuchen Sie es erneut.",
 					"WARNING");
-		}
-		else if (datumVonDatePicker.getValue().toString().isEmpty()) {
+		} else if (datumVonDatePicker.getValue().toString().isEmpty()) {
 			alert = new AlertUtil("Datum ungültig",
-					"Es wurde kein gültiges Datum eingegeben. Bitte geben Sie einen validen Datum an und versuchen Sie es erneut.",
+					"Es wurde kein gültiges Startdatum eingegeben. Bitte geben Sie ein valides Startdatum an und versuchen Sie es erneut.",
 					"WARNING");
-		}
-		else if (datumBisDatePicker.getValue().toString().isEmpty()) {
+		} else if (datumBisDatePicker.getValue().toString().isEmpty()) {
 			alert = new AlertUtil("Datum ungültig",
-					"Es wurde kein gültiges Datum eingegeben. Bitte geben Sie einen validen Datum an und versuchen Sie es erneut.",
+					"Es wurde kein gültiges Enddatum eingegeben. Bitte geben Sie ein valides Enddatum an und versuchen Sie es erneut.",
 					"WARNING");
 		}
-		
+
 		else if (stdZahlTextField.getText().isEmpty()) {
 			alert = new AlertUtil("Stundenanzahl ungültig",
-					"Es wurde kein gültiger Stundenanzahl eingegeben. Bitte geben Sie einen validen Stundenanzahl an und versuchen Sie es erneut.",
+					"Es wurde keine gültige Stundenanzahl eingegeben. Bitte geben Sie eine valide Stundenanzahl an und versuchen Sie es erneut.",
 					"WARNING");
-		}
-		else {
-			
+		} else {
+
 			try {
 				if (event == null) {
 					event = new Event();
@@ -151,34 +143,32 @@ public class VeranstaltungController {
 				this.event.setVfg(vfgDatePicker.getValue().toString());
 				if (event.getId() == 0) {
 					MainPageController.eventDAO.insertEvent(event);
-				}
-				else {
+				} else {
 					MainPageController.eventDAO.updateEvent(event);
 				}
 				FXML_GUI.primaryStage.close();
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	@FXML
 	private void handleCancel() {
-		
+
 		FXML_GUI.primaryStage.close();
 	}
-	
+
 	@FXML
 	private void handleTypingDozent() {
-		
+
 		// TODO Vorschläge bei Eingabe des Dozenten (list)
-		
+
 	}
-	
+
 	@FXML
 	private void handleCounting() {
-		
+
 		double rechnung = Double.valueOf(euro_StdComboBox.getValue()) * Double.valueOf(stdZahlTextField.getText());
 		betragTextField.setText("" + rechnung);
 	}
