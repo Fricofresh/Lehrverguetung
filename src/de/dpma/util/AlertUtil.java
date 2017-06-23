1b2a590455298fff32bc21cdded09ce549db22ea
Flo <Flo@DESKTOP-R6OOO5U>
2017-06-23 08:18:02 +0200

    AlertUtil umfassender gemacht: ERROR, CONFIRMATION und NONE

--- src/de/dpma/util/AlertUtil.java
+++ src/de/dpma/util/AlertUtil.java
@@ -34,6 +34,37 @@ public class AlertUtil {
 			alert.setContentText(content);
 			// alert.initOwner(stage);
 			alert.showAndWait();
+		} else if (type.equalsIgnoreCase("error") || type.equalsIgnoreCase("err") || type.equalsIgnoreCase("e")) {
+			Alert alert = new Alert(AlertType.ERROR);
+
+			DialogPane dialogPane = alert.getDialogPane();
+
+			alert.setTitle("Fehler");
+			alert.setHeaderText(title);
+			alert.setContentText(content);
+			// alert.initOwner(stage);
+			alert.showAndWait();
+		} else if (type.equalsIgnoreCase("confirmation") || type.equalsIgnoreCase("confirm")
+				|| type.equalsIgnoreCase("conf") || type.equalsIgnoreCase("c")) {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+
+			DialogPane dialogPane = alert.getDialogPane();
+
+			alert.setTitle("Bestätigung");
+			alert.setHeaderText(title);
+			alert.setContentText(content);
+			// alert.initOwner(stage);
+			alert.showAndWait();
+		} else {
+			Alert alert = new Alert(AlertType.NONE);
+
+			DialogPane dialogPane = alert.getDialogPane();
+
+			alert.setTitle("Meldung");
+			alert.setHeaderText(title);
+			alert.setContentText(content);
+			// alert.initOwner(stage);
+			alert.showAndWait();
 		}
 	}
 }
