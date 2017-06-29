d2e7325049a48b6095de39565da2ce1dde69d0f2
Flo <Flo@DESKTOP-R6OOO5U>
2017-06-29 09:51:49 +0200

    Bild
    Flo

--- src/de/dpma/FXML_GUI.java
+++ src/de/dpma/FXML_GUI.java
@@ -13,47 +13,49 @@ import de.dpma.view.RootLayoutController;
 import de.dpma.view.VeranstaltungController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class FXML_GUI {
-	
+
 	Logger log = Logger.getLogger(FXML_GUI.class.getName());
-	
+
 	public static Stage[] primaryStage = new Stage[100];
-	
+
 	public BorderPane rootLayout;
-	
+
 	Scene scene;
-	
+
 	AlertUtil alert;
-	
+
 	Object tabelle;
-	
+
 	public FXML_GUI() {
-		
+
 	}
-	
+
 	public FXML_GUI(Stage primaryStage, BorderPane rootLayout) {
-		
+
 		MainApp.counter++;
 		this.rootLayout = rootLayout;
-		
+
 		initRootLayout(true);
 	}
-	
+
 	// public FXML_GUI(Stage primaryStage, BorderPane rootLayout, String dump) {
 	// this.primaryStage[MainApp.counter] = primaryStage;
 	// this.rootLayout = rootLayout;
 	// initRootLayout(true);
 	// }
-	
+
 	public void initRootLayout(boolean b) {
-		
+
 		try {
 			primaryStage[MainApp.counter] = new Stage();
 			FXMLLoader loader = new FXMLLoader();
+			primaryStage[MainApp.counter].getIcons().add(new Image("file:ressources/euro.png"));
 			loader.setLocation(FXML_GUI.class.getResource("view/RootLayout.fxml"));
 			rootLayout = (BorderPane) loader.load();
 			Scene scene = new Scene(rootLayout);
@@ -63,133 +65,123 @@ public class FXML_GUI {
 				root.gesammtListeMenuItem.setVisible(false);
 				root.einstellungenMenuItem.setVisible(false);
 			}
-			
+
 			primaryStage[MainApp.counter].setScene(scene);
-			
+
 			primaryStage[MainApp.counter].show();
-			
-		}
-		catch (NullPointerException n) {
+
+		} catch (NullPointerException n) {
 			String nu = n.toString();
 			alert = new AlertUtil("NullPointerException", nu, "WARNING");
 			n.printStackTrace();
-		}
-		catch (Exception e) {
-			
+		} catch (Exception e) {
+
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void showMainPage() {
-		
+
 		handleChildren("MainPage");
-		
+
 	}
-	
+
 	// Anpassung der Größe
 	private void sizeHandling(double height, double width) {
-		
+
 		primaryStage[MainApp.counter].setMinWidth(width + 60);
 		primaryStage[MainApp.counter].setMinHeight(height + 120);
 	}
-	
+
 	public void showDozent(Object tabelle) {
-		
+
 		if (tabelle != null) {
 			handleChildren("Dozent", tabelle, "dc");
-		}
-		else {
+		} else {
 			handleChildren("Dozent");
 		}
 	}
-	
+
 	public void showVeranstaltung(Object tabelle) {
-		
+
 		// Path currentRelativePath = Paths.get("");
 		// String s = currentRelativePath.toAbsolutePath().toString();
 		// System.out.println("Current relative path is: " + s);
-		
+
 		if (tabelle != null) {
 			this.tabelle = tabelle;
 			handleChildren("Veranstaltung", tabelle, "vc");
-		}
-		else {
-			
+		} else {
+
 			handleChildren("Veranstaltung");
 		}
-		
+
 	}
-	
+
 	public void showLehrverguetungssaetze(Object tabelle) {
-		
+
 		if (tabelle != null) {
 			handleChildren("Lehrverguetungssaetze", tabelle, "lc");
-		}
-		else {
+		} else {
 			handleChildren("Lehrverguetungssaetze");
 		}
 		primaryStage[MainApp.counter].setMaxWidth(400 + 60);
 		primaryStage[MainApp.counter].setMaxHeight(250 + 120);
 	}
-	
+
 	private void handleChildren(String FXML_Name) {
-		
+
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(this.getClass().getResource("view/" + FXML_Name + ".fxml"));
-			
+
 			AnchorPane children;
 			children = (AnchorPane) loader.load();
 			sizeHandling(children.getPrefHeight(), children.getPrefWidth());
 			rootLayout.setCenter(children);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	private void handleChildren(String FXML_Name, Object tabelle, String check) {
-		
+
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(this.getClass().getResource("view/" + FXML_Name + ".fxml"));
-			
+
 			AnchorPane children;
 			children = (AnchorPane) loader.load();
 			if (check.equals("vc")) {
 				Event event = (Event) tabelle;
 				VeranstaltungController vc = loader.getController();
 				vc.handleNew(event);
-			}
-			else if (check.equals("dc")) {
+			} else if (check.equals("dc")) {
 				Dozent dozent = (Dozent) tabelle;
 				DozentController dc = loader.getController();
 				dc.handleNew(dozent);
-			}
-			else if (check.equals("lc")) {
+			} else if (check.equals("lc")) {
 				Stundenlohn stundenlohn = (Stundenlohn) tabelle;
 				LehrverguetungssaetzeController lc = loader.getController();
 				lc.handleNew(stundenlohn);
-			}
-			else if (check.equals("confIni")) {
+			} else if (check.equals("confIni")) {
 				Event event = (Event) tabelle;
 				InsertPersonalDataController sipd = loader.getController();
 				sipd.setEvent(event);
 			}
 			sizeHandling(children.getPrefHeight(), children.getPrefWidth());
 			rootLayout.setCenter(children);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void showConfigIni(Object tabelle) {
-		
+
 		handleChildren("InsertPersonalData", tabelle, "confIni");
-		
+
 	}
 }
